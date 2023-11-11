cb6523e7f39b1414f0fe2800e231ec272a660493
dev-muhammadabid <muhammadabid.dev@gmail.com>
2023-11-11 19:12:27 +0530

    90% Completet

--- index.js
+++ index.js
@@ -26,3 +26,22 @@ function changeFont() {
 }
 
 setInterval(changeFont, 1000);
+
+//SMTP Setup
+function sendEmail() {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "muhammadabid.dev@gmail.com",
+    Password: "1718C80D7F6D3516282BC04D0EC341F5F788",
+    To: "personal.abidzaeem@gmail.com",
+    From: "muhammadabid.dev@gmail.com",
+    Subject: "New Contact Form",
+    Body:
+      "Name: " +
+      document.getElementById("contact_name").value +
+      "<br> Email: " +
+      document.getElementById("contact_email").value +
+      "<br> Phone No.: " +
+      document.getElementById("contact_number").value,
+  }).then((message) => alert("We Recieved Your Approach"));
+}
